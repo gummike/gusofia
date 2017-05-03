db26f04223313ff034471ad19488807e265351dc
顾利东 <gulidong@gulidongdeMacBook-Pro.local>
2017-05-03 11:17:25 +0800

    singleton

--- gusofia-pattern/src/main/java/pattern/singleton/SingletonStaticHolder.java
+++ gusofia-pattern/src/main/java/pattern/singleton/SingletonStaticHolder.java
@@ -10,7 +10,7 @@ import pattern.Instance;
 public class SingletonStaticHolder {
 
     private static class InstanceHolder{
-        private static Instance instance = new Instance();
+        private static Instance instance = new Instance(); // 不能加final ，加了就不是延迟初始化了
     }
 
     public static Instance getInstance(){
